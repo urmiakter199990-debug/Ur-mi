8f2b37ca9e9841031b2083669310030394b9ef77
urmiakter199990-debug <urmiakter199990@gmail.com>
2025-08-13 21:28:58 +0600

    Update bot.js

--- Script/commands/bot.js
+++ Script/commands/bot.js
@@ -71,7 +71,7 @@ module.exports.handleEvent = async function({ api, event, args, Threads, Users }
    };
 
   if ((event.body.toLowerCase() == "admin") || (event.body.toLowerCase() == "boter admin")) {
-     return api.sendMessage("He is লিখন ッ❤️ তাকে সবাই উল্লাস নামে  চিনে🤙", threadID);
+     return api.sendMessage("He is লিখন ッ❤️ তাকে সবাই লিখন নামে  চিনে🤙", threadID);
    };
 
    if ((event.body.toLowerCase() == "ai") || (event.body.toLowerCase() == "Ai")) {
